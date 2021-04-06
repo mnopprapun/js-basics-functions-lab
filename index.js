44023f0e69895627b302dda40fb05b35365c6890
Marc Nopprapun <mnopprapun@gmail.com>
2021-04-06 13:54:48 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const hq = 42
+
+function distanceFromHqInBlocks(start) {
+    return (Math.abs(hq - start))
+}
+
+function distanceFromHqInFeet(feet) {
+    return (Math.abs(hq - feet) * 264)
+}
+
+function distanceTravelledInFeet(begin, end){
+    return (Math.abs(begin - end) * 264 );
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination); 
+if (distance <= 400){
+    return 0;
+}
+else if (distance > 400 && distance <= 2000){
+return .02 * (distance - 400)}
+else if (distance >= 2000 && distance <=2500) {return 25}
+else if (distance > 2500) {return 'cannot travel that far'}
+}
